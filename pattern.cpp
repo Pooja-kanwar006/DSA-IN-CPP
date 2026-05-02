147258327f5910a13584cb918c5c2039ba6563e4
pooja-kanwar006 <poojakanwarbhati24@gmail.com>
2026-05-02 11:20:00 +0530

    c++file

---
+++ pattern.cpp
@@ -0,0 +1,18 @@
+// rantangle of *
+#include <iostream> // 1. Preprocessor directive
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i <3; )
+    {
+        for (int j = 0; j <5, j++;)
+        {
+            cout << "*";
+      
+            
+        };
+        
+    }
+    return 0; // 4. Success statu
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+   for (int i = 0; i = 3, i++;)
+    {
+        for (int j = 0; j = 5; j++)
+        {
+            cout << 8;
+            
+        };
+        cout<<endl;
+    }
