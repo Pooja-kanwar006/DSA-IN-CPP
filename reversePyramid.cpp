c73a1f14eb7ea39bf45f3b10a5d0d390e1992fa8
pooja-kanwar006 <poojakanwarbhati24@gmail.com>
2026-05-07 17:48:27 +0530

    add reverse pyramid cpp

---
+++ reversePyramid.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int rows;
+cout<<"enter no of rows: "<<endl;
+cin>>rows;
+for(int i=0;i<rows;i++){
+for(int j=0;j<rows;j++){
+if(j<i){
+cout<<" ";}else{cout<<"* ";}
+
+}cout<<endl;
+}
+
+
+return 0;}
