c9e7a72c80376c54ea9bc6222933c7e82a0953c8
pooja-kanwar006 <poojakanwarbhati24@gmail.com>
2026-05-08 14:37:52 +0530

    update reverse Pyramid

--- reversePyramid.cpp
+++ reversePyramid.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 using namespace std;
-int main(){
-int rows;
-cout<<"enter no of rows: "<<endl;
-cin>>rows;
-for(int i=0;i<rows;i++){
-for(int j=0;j<rows;j++){
-if(j<i){
-cout<<" ";}else{cout<<"* ";}
+int main()
+{
+    int rows;
+    cout << "enter no of rows: " << endl;
+    cin >> rows;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            if (j < i)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
 
-}cout<<endl;
-}
 
-
-return 0;}
+        return 0;
+    }
